e73a22a90106eb6d6f6c39052ca7f817f5372d1b
Jairo Revilla <revilla.jairo@gmail.com>
2025-08-10 20:33:40 -0500

    uso de stack & heap

---
+++ src/jrevilla/play/MainStackHeap.java
@@ -0,0 +1,16 @@
+package jrevilla.play;
+
+import jrevilla.play.contenido.Pelicula;
+
+public class MainStackHeap {
+    public static void main(String[] args) {
+        Pelicula reyLeon = new Pelicula("El Rey León", 135, "Animada");
+        Pelicula harryPotter = new Pelicula("Harry Potter", 200, "Fantasía");
+
+        reyLeon = harryPotter;
+        reyLeon.titulo = "El Hobbit";
+        
+        System.out.println("reyLeon: " + reyLeon.titulo);
+        System.out.println("harryPotter: " + harryPotter.titulo);
+    }
+}
